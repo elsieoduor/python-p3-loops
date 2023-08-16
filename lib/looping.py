5a15a17923267f1264a7f1be3871aead27c5260f
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-16 15:35:55 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 0
+    while i < 10:
+        i += 1
+        return('Happy New Year!')
+        
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [num * num for num in int_list]
+    return squared_list
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 100):
+        if num % 3 == 0 and num % 5 == 0:
+            return('FizzBuzz')
+        elif num % 3:
+            return('Fizz')
+        elif num % 5:
+            return('Buzz')
+        else:
+            return(num)
+        
+
